80645a02ce839dda74f8e55977376da0402a0ae3
root <root@DESKTOP-IQFTOA7>
2023-01-27 21:36:19 -0500

    pushing neo4j and EC2 stuff

--- infra/ec2_config.bash
+++ infra/ec2_config.bash
@@ -11,28 +11,40 @@ cd EventApp
 git checkout Develop_1.1
 
 
-sudo amazon-linux-extras install epel
-sudo yum install xrdp
-sudo systemctl start xrdp
+# sudo amazon-linux-extras install epel
+# sudo yum install xrdp
+# sudo systemctl start xrdp
 
 ## https://aws.amazon.com/premiumsupport/knowledge-center/ec2-linux-2-install-gui/
-sudo amazon-linux-extras install mate-desktop1.x
-sudo bash -c 'echo PREFERRED=/usr/bin/mate-session > /etc/sysconfig/desktop'
-sudo yum install tigervnc-server
+# sudo amazon-linux-extras install mate-desktop1.x
+# sudo bash -c 'echo PREFERRED=/usr/bin/mate-session > /etc/sysconfig/desktop'
+# sudo yum install tigervnc-server
 
-## Set password When asked if you want to enter a view-only password, press "n"
-vncpasswd
+# ## Set password When asked if you want to enter a view-only password, press "n"
+# vncpasswd
 
-sudo mkdir /etc/tigervnc
-sudo bash -c 'echo localhost > /etc/tigervnc/vncserver-config-mandatory'
-sudo cp /lib/systemd/system/vncserver@.service /etc/systemd/system/vncserver@.service
-sudo sed -i 's/<USER>/ec2-user/' /etc/systemd/system/vncserver@.service
-sudo systemctl daemon-reload
-sudo systemctl enable vncserver@:1
-sudo systemctl start vncserver@:1
+# sudo mkdir /etc/tigervnc
+# sudo bash -c 'echo localhost > /etc/tigervnc/vncserver-config-mandatory'
+# sudo cp /lib/systemd/system/vncserver@.service /etc/systemd/system/vncserver@.service
+# sudo sed -i 's/<USER>/ec2-user/' /etc/systemd/system/vncserver@.service
+# sudo systemctl daemon-reload
+# sudo systemctl enable vncserver@:1
+# sudo systemctl start vncserver@:1
 
 ## install browser
 
 
+# https://neo4j.com/developer/neo4j-cloud-aws-ec2-ami/
+export GROUP="neo4j-sg"
+aws ec2 create-security-group \
+  --group-name $GROUP \
+  --description "Neo4j security group"
 
+for port in 22 7474 7473 7687; do
+  aws ec2 authorize-security-group-ingress --group-name $GROUP --protocol tcp --port $port --cidr 0.0.0.0/0
+done
 
+aws ec2 describe-images \
+   --region us-east-1 \
+   --owner 385155106615 \
+   --query "Images[*].{ImageId:ImageId,Name:Name}"
---
+++ infra/neo4j.bash
@@ -0,0 +1,3 @@
+ssh -i "Neo4j_key.pem" ec2-user@100.26.167.168
+
+sudo service neo4j start
