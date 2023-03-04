bd45db43ab95eb3e81e3ff92b6df9ec8d38fa924
EC2 Default User <ec2-user@ip-172-31-61-153.ec2.internal>
2023-03-04 07:45:17 +0000

    still working. just commiting before i shift over from event-app to stonks

---
+++ infra/stonks_lambdas/get_node/scripts/utils/aws_handler.py
@@ -0,0 +1,37 @@
+import json
+
+import boto3
+from botocore.exceptions import ClientError
+
+from utils.logger import Logger
+
+
+class AWSHandler:
+    def __init__(self, logger: Logger=None):
+        if logger is None:
+            logger = Logger(__name__)
+        self.logger = logger
+        self.session = boto3.session.Session()
+        self.region_name = "us-east-1"
+        
+    def get_secret(self, secret_name: str=None):
+        try:
+            client = self.session.client(
+                service_name='secretsmanager',
+                region_name=self.region_name
+            )
+            secrets = client.get_secret_value(
+                SecretId=secret_name
+            )
+
+            secrets = json.loads(secrets['SecretString'])
+            self.logger.info(msg=f'Successfully retrieved secrets')
+            
+            return secrets
+            
+        except ClientError as e:
+            raise e
+            
+        except Exception as error:
+            self.logger.error(msg=error)
+            raise ValueError(error)
