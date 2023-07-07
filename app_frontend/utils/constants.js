d7856a102b9e51fa6c59b15cd3fdc8d389415421
EC2 Default User <ec2-user@ip-172-31-61-153.ec2.internal>
2023-07-07 02:20:28 +0000

    icon mappings

--- app_frontend/utils/constants.js
+++ app_frontend/utils/constants.js
@@ -1,5 +1,13 @@
+import comedy_icon from '../assets/event_type_icons/Comedy.png';
+import health_fitness_wellness_icon from '../assets/event_type_icons/HealthFitnessWellness.png';
+
 
 export const day_start_time = '00:00:00';
 export const day_end_time = '23:59:59';
 export const day_format = 'yyyy-MM-dd';
 export const date_time_format = "yyyy-MM-dd'T'HH:mm:ss";
+
+export const event_types_icon_map = {
+    "Comedy": comedy_icon,
+    "Health, Fitness, & Wellness": health_fitness_wellness_icon
+};
