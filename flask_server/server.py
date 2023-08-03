9d81b0baa6307000e848d81df60025058f99aa2c
EC2 Default User <ec2-user@ip-172-31-61-153.ec2.internal>
2023-08-03 00:29:31 +0000

    pushing before nginx attempt

--- flask_server/server.py
+++ flask_server/server.py
@@ -58,59 +58,44 @@ def get_user_login_info():
 @app.route("/get_event_type_mappings", methods=["GET"])
 def get_event_type_mappings():
     try:
-        data = request.get_json()  # Get data sent in POST request
-        if not data:
-            return jsonify({"message": "No input data provided"}), 400
-
-        result = neo4j.execute_query(queries.GET_EVENT_TYPE_MAPPINGS)
-        
+        result = neo4j.execute_query(queries.GET_EVENT_TYPE_NAMES_MAPPINGS)
         return jsonify(result)
-
     except Exception as e:
         # Return a 500 error with a description of the problem
         return jsonify({"message": "An error occurred: " + str(e)}), 500
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, ssl_context=('certification/cert.pem', 'certification/key.pem'))
-
-
 @app.route("/events")
 def events():
-    # FETCH_EVENTS_FOR_MAP = """
-    #     MATCH (event:Event)
-    #     WHERE "2023-07-06T00:00:00" <= event.StartTimestamp <= "2023-07-06T23:59:59"
-    #     OPTIONAL MATCH (event)-[r:ATTENDING]-()
-    #     WITH event, count(r) as AttendeeCount
-    #     MATCH (eventType:EventType)-[:RELATED_EVENT]->(event)
+    FETCH_EVENTS_FOR_MAP = """
+        MATCH (event:Event)
+        WHERE "2023-07-06T00:00:00" <= event.StartTimestamp <= "2023-07-06T23:59:59"
+        OPTIONAL MATCH (event)-[r:ATTENDING]-()
+        WITH event, count(r) as AttendeeCount
+        MATCH (eventType:EventType)-[:RELATED_EVENT]->(event)
 
-    #     RETURN
-    #         event.Address as Address,
-    #         event.CreatedByUUID as CreatedByUUID,
-    #         event.Host as Host,
-    #         event.Lon as Lon,
-    #         event.Lat as Lat,
-    #         event.StartTimestamp as StartTimestamp,
-    #         event.EndTimestamp as EndTimestamp,
-    #         event.EventName as EventName,
-    #         event.UUID as UUID,
-    #         event.EventURL as EventURL,
-    #         event.Price as Price,
-    #         event.FreeEventFlag as FreeEventFlag,
-    #         event.EventTypeUUID as EventTypeUUID,
-    #         eventType.EventType as EventType,
-    #         eventType.IconURI as EventTypeIconURI,
-    #         AttendeeCount;
-    #     """
-    # result = neo4j.execute_query(FETCH_EVENTS_FOR_MAP)
-    # print(result)
-    result = {
-        "Email": "matt@gmail.com",
-        "Username": "bigolehealz",
-        "FirstName": "Matt",
-        "PasswordHash": "4f31fa50e5bd5ff45684e560fc24aeee527a43739ab611c49c51098a33e2b469",
-        "LastName": "Smith",
-        "UUID": "87913a2f-67d9-4d37-b863-dfea86e2c207"
-    }
-    return {"events": result}
+        RETURN
+            event.Address as Address,
+            event.CreatedByUUID as CreatedByUUID,
+            event.Host as Host,
+            event.Lon as Lon,
+            event.Lat as Lat,
+            event.StartTimestamp as StartTimestamp,
+            event.EndTimestamp as EndTimestamp,
+            event.EventName as EventName,
+            event.UUID as UUID,
+            event.EventURL as EventURL,
+            event.Price as Price,
+            event.FreeEventFlag as FreeEventFlag,
+            event.EventTypeUUID as EventTypeUUID,
+            eventType.EventType as EventType,
+            eventType.IconURI as EventTypeIconURI,
+            AttendeeCount;
+        """
+    result = neo4j.execute_query(FETCH_EVENTS_FOR_MAP)
 
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
