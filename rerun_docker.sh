92a7ac75dcc0832d4f91af18781a053775cc3ca6
EC2 Default User <ec2-user@ip-172-31-61-153.ec2.internal>
2023-08-06 20:32:58 +0000

    backend is working

--- rerun_docker.sh
+++ rerun_docker.sh
@@ -2,22 +2,15 @@
 
 docker-compose down
 
-flask_server_dir="flask_server"
+flask_server_dir="flask-server"
 db_dir="db"
 utils_dir="utils"
 
-
-# if lsof -Pi :5001 -sTCP:LISTEN -t >/dev/null ; then
 kill -9 $(lsof -t -i:5001)
 sudo kill -9 $(sudo lsof -t -i:5001)
-# fi
 
-# if lsof -Pi :19006 -sTCP:LISTEN -t >/dev/null ; then
 kill -9 $(lsof -t -i:19006)
 sudo kill -9 $(sudo lsof -t -i:19006)
-# fi
-
-#!/bin/bash
 
 if ! docker info >/dev/null 2>&1; then
     echo "Starting Docker..."
@@ -34,7 +27,6 @@ fi
 docker stop $(docker ps -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -a -q)
-
 
 docker container prune -f
 docker image prune -f
