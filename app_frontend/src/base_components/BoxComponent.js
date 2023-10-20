3af864bce6ebaca6626cd192b2e0e782599dae3f
bigolehealz <matt.t.healy1994@gmail.com>
2023-10-20 19:37:57 +0000

    pushing before i mess with sessions

--- app_frontend/src/base_components/BoxComponent.js
+++ app_frontend/src/base_components/BoxComponent.js
@@ -3,10 +3,10 @@ import Box from "@mui/material/Box";
 import { common_styles } from '../styles';
 
 const BoxComponent = React.forwardRef((props, ref) => {
-  const { children, style, ...otherProps } = props;
+  const { children, sx, style, ...otherProps } = props;
   
   return (
-    <Box ref={ref} sx={{...common_styles.basicComponent, ...style}} {...otherProps}>
+    <Box ref={ref} sx={{...common_styles.basicComponent, ...style, ...sx}} {...otherProps}>
       {children}
     </Box>
   );
--- app_frontend/src/pages/LoginPage.js
+++ app_frontend/src/pages/LoginPage.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { useGoogleLogin } from '@react-oauth/google';
 import { ToastContainer } from 'react-toastify';
+import Button from '@mui/material/Button';
+import Typography from "@mui/material/Typography";
 
+import BoxComponent from '../base_components/BoxComponent';
 import { ButtonComponent } from '../base_components/ButtonComponent';
 import { TextComponent } from '../base_components/TextComponent';
 import {
@@ -10,7 +13,7 @@ import {
   UserSessionContext
 } from '../utils/Contexts';
 import { useFetchGoogleProfile, useSetUserProfile } from '../utils/Hooks';
-import { login_page_styles } from '../styles';
+import { login_page_styles, button_styles, common_styles } from '../styles';
 
 export function LoginPage() {
 
@@ -19,8 +22,8 @@ export function LoginPage() {
   const { create_user_profile_context, setCreateUserProfileContext } = React.useContext(CreateUserProfileContext);
 
 
-  const [ fetching_google_profile, setFetchingGoogleProfile ] = useState(false);
-  const [ google_access_token, setGoogleAccessToken ] = useState(null);
+  const [fetching_google_profile, setFetchingGoogleProfile] = useState(false);
+  const [google_access_token, setGoogleAccessToken] = useState(null);
 
 
   const get_google_profile = (response) => {
@@ -44,24 +47,44 @@ export function LoginPage() {
     setUserSession,
     resetLoginInfo,
     // logger
-    );
+  );
 
   return (
-    <div style={login_page_styles.verticalContainer} id="LoginFullPageContainer">
-      <ToastContainer />
-      <div style={login_page_styles.horizontalContainer}>
-        <div>
-          <TextComponent style={login_page_styles.title}>
-            Login
-          </TextComponent>
-        </div>
-        <ButtonComponent
-          title="Sign in with Google"
-          onPress={() => login()}
-          style={{ backgroundColor: '#bbbbbb', color: '#222222'}}
+    <BoxComponent
+      id="box-login-vertical"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh'
+      }} >
+      <BoxComponent style={login_page_styles.horizontalContainer}>
+        <Typography
+          variant="h5"
+          noWrap
+          component="a"
+          href="/"
+          sx={{
+            mr: 2,
+            display: "flex",
+            flexGrow: 1,
+            textAlign: "center",
+            fontWeight: 700,
+            color: "inherit",
+            textDecoration: "none",
+            margin: "5px",
+          }}
         >
-        </ButtonComponent>
-      </div>
-    </div>
+          Login
+        </Typography>
+        <Button
+          onClick={() => login()}
+          sx={{ ...button_styles.menu_button_styles, margin: "5px" }}
+        >
+          Sign in with Google
+        </Button>
+      </BoxComponent>
+    </BoxComponent>
   );
 };
