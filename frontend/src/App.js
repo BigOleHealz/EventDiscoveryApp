c672096945b49182fbcdb4066dd2eec575932523
bigolehealz <matt.t.healy1994@gmail.com>
2024-02-08 18:47:55 -0500

    endpoint working?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
+import { useFetchGoogleMapsApiKey } from './utils/Hooks';
 
 function App() {
 
@@ -10,33 +11,7 @@ function App() {
   const [fetching_google_maps_api_key, setFetchingGoogleMapsApiKey] = useState(true);
   const [google_maps_api_key, setGoogleMapsApiKey] = useState(null);
 
-  useEffect(() => {
-    if (fetching_google_maps_api_key) {
-      fetch('/api/get_secret', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          secret_id: 'GOOGLE_MAPS_API_KEY',
-        }),
-      }).then(res => res.json())
-        .then(data => {
-          console.log('data:', data);
-          if (data) {
-            setGoogleMapsApiKey(data.GOOGLE_MAPS_API_KEY);
-          }
-        }).catch((error) => {
-          console.error('Error:', error);
-        });
-      setFetchingGoogleMapsApiKey(false);
-    }
-  }, [
-    fetching_google_maps_api_key,
-    setGoogleMapsApiKey,
-    setFetchingGoogleMapsApiKey,
-  ]);
-
+  useFetchGoogleMapsApiKey(fetching_google_maps_api_key, setGoogleMapsApiKey, setFetchingGoogleMapsApiKey);
 
 
   return (
