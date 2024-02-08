fdd9f87d58034e7f7a881c5bfbc04ae785035409
bigolehealz <matt.t.healy1994@gmail.com>
2024-02-08 18:31:18 -0500

    trying to get backend working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
-import { Deploy } from './Component/Deploy/Deploy';
 import { useState, useEffect } from 'react';
 
 function App() {
 
   const [state, setState] = useState({});
 
+
+  const [fetching_google_maps_api_key, setFetchingGoogleMapsApiKey] = useState(true);
+  const [google_maps_api_key, setGoogleMapsApiKey] = useState(null);
+
   useEffect(() => {
-    fetch('/api').then(response => {
-      if (response.status == 200) {
-        return response.json();
-      }
-    }).then(data => setState(data))
-    .then(error => console.log(error))
-  }, []);
+    if (fetching_google_maps_api_key) {
+      fetch('/api/get_secret', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          secret_id: 'GOOGLE_MAPS_API_KEY',
+        }),
+      }).then(res => res.json())
+        .then(data => {
+          console.log('data:', data);
+          if (data) {
+            setGoogleMapsApiKey(data.GOOGLE_MAPS_API_KEY);
+          }
+        }).catch((error) => {
+          console.error('Error:', error);
+        });
+      setFetchingGoogleMapsApiKey(false);
+    }
+  }, [
+    fetching_google_maps_api_key,
+    setGoogleMapsApiKey,
+    setFetchingGoogleMapsApiKey,
+  ]);
+
+
 
   return (
     <div className="App">
-      <Deploy prop={state}/>
+      {google_maps_api_key ? (
+        <div>
+          {google_maps_api_key}
+        </div>) : (
+        <div>
+          Loading...
+        </div>
+      )
+      }
     </div>
   );
 }
