3e4d30be1f63b910a7a3158f90bf56c956efd4bd
bigolehealz <matt.t.healy1994@gmail.com>
2024-02-08 16:48:43 -0500

    new build

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: guicorn app
+web: guicorn app:app
---
+++ my-app/src/Component/Deploy/Deploy.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Deploy = ({prop}) => {
+  return (
+    <div>
+      <h1>{prop.tutorial}</h1>
+    </div>
+  );
+}
