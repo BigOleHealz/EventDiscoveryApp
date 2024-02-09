fc3239357a2338798fd1c2646cf3487dae9f32a0
bigolehealz <matt.t.healy1994@gmail.com>
2024-02-09 11:34:31 -0500

    initial git commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,25 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import { AppHandler } from './AppHandler';
-import ErrorBoundary from './utils/ErrorBoundary';
-import './styles/globals.css';
+// import { AppHandler } from './AppHandler';
+// import ErrorBoundary from './utils/ErrorBoundary';
+// import './styles/globals.css';
 
 
 function MainComponent() {
   return (
-    <Router>
-      <AppHandler />
-    </Router>
+    // <Router>
+    //   <AppHandler />
+    // </Router>
+    <div>
+      <h1>Test</h1>
+    </div>
   );
 }
 
 export default function App() {
   return (
-    <ErrorBoundary>
+    // <ErrorBoundary>
       <MainComponent />
-    </ErrorBoundary>
+    // </ErrorBoundary>
   );
 }
