4ef27b7d8eb6834001deba1860ad2c9fe1aeee60
bigolehealz <matt.t.healy1994@gmail.com>
2024-02-15 17:13:07 -0500

    removing Dockerfiles

--- run_docker.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-docker stop neo4j-cont
-docker rm neo4j-cont
-docker rmi neo4j
-
-docker build -t neo4j .
-docker run -d --name neo4j-cont -p 7474:7474 -p 7687:7687 neo4j
-docker logs neo4j-cont
